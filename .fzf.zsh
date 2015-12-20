32ac7d0ccc13f23325765117185adf46683a85b5
Ashay Humane <ashay.humane@gmail.com>
2015-12-19 17:32:04 -0800

    add fzf custom config

---
+++ .fzf.zsh
@@ -0,0 +1,23 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" =~ "/home/ashay/.fzf/bin" ]]; then
+  export PATH="$PATH:/home/ashay/.fzf/bin"
+fi
+
+# Man path
+# --------
+if [[ ! "$MANPATH" =~ "/home/ashay/.fzf/man" && -d "/home/ashay/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:/home/ashay/.fzf/man"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- =~ i ]] && source "/home/ashay/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/ashay/.fzf/shell/key-bindings.zsh"
+
+export FZF_DEFAULT_OPTS="-e"
+
+export FZF_TMUX=0
