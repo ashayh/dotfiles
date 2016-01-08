924310242391d95129b15dfd93e94c41d4e41fc9
Ashay Humane <ashay.humane@gmail.com>
2016-01-07 21:21:19 -0800

    add color config for diff-highlight to git

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,23 +1,20 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" =~ "$HOME/.fzf/bin" ]]; then
-  export PATH="$PATH:$HOME/.fzf/bin"
+if [[ ! "$PATH" == */home/ashay/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/ashay/.fzf/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" =~ "$HOME/.fzf/man" && -d "$HOME/.fzf/man" ]]; then
-  export MANPATH="$MANPATH:$HOME/.fzf/man"
+if [[ ! "$MANPATH" == */home/ashay/.fzf/man* && -d "/home/ashay/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:/home/ashay/.fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- =~ i ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/ashay/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/.fzf/shell/key-bindings.zsh"
+source "/home/ashay/.fzf/shell/key-bindings.zsh"
 
-export FZF_DEFAULT_OPTS="-e"
-
-export FZF_TMUX=0
